11708fa17bcd3d9f806b66cbeb4cee6d64d97a95
Your Name <you@example.com>
2026-09-20 17:12:40 +0800

    Exercise 2: Add core React Native primitives

--- App.tsx
+++ App.tsx
@@ -1,11 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Pressable,
+} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>DOrSU Digital Campus Pass</Text>
+
+      <Image
+        source={require('./assets/icon.png')}
+        style={styles.image}
+      />
+
+      <Text style={styles.description}>
+        Student Verification System
+      </Text>
+
+      <Pressable
+        style={styles.button}
+        onPress={() => alert('Campus Pass Verified!')}
+      >
+        <Text style={styles.buttonText}>Verify Campus Pass</Text>
+      </Pressable>
     </View>
   );
 }
@@ -13,8 +34,36 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+
+  title: {
+    fontSize: 24,
+    fontWeight: '800',
+    textAlign: 'center',
+  },
+
+  image: {
+    width: 120,
+    height: 120,
+    marginVertical: 20,
+  },
+
+  description: {
+    fontSize: 16,
+    marginBottom: 20,
+  },
+
+  button: {
+    padding: 14,
+    borderRadius: 10,
+    backgroundColor: '#00758F',
+  },
+
+  buttonText: {
+    color: '#FFFFFF',
+    fontWeight: '700',
   },
 });
